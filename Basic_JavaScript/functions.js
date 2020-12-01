6650d9a5bcebb8d6299101d82eee69b0250e4272
LuisContrerasGlz <luis.fco.contreras@gmail.com>
2020-11-30 23:49:41 -0600

    progress on functions file

--- Basic_JavaScript/functions.js
+++ Basic_JavaScript/functions.js
@@ -9,3 +9,17 @@ function reusableFunction() {
   }
   
   reusableFunction();
+
+  /* Parameters are variables that act as placeholders for the values that are to be input to a function when it is called. 
+  When a function is defined, it is typically defined along with one or more parameters. 
+  The actual values that are input (or "passed") into a function when it is called are known as argument*/
+
+  /* 
+1. Create a function called functionWithArgs that accepts two arguments and outputs their sum to the dev console.
+2. Call the function with two numbers as arguments.*/
+
+function functionWithArgs(num1, num2) {
+    console.log(num1 + num2);
+  }
+
+  functionWithArgs(10,5);

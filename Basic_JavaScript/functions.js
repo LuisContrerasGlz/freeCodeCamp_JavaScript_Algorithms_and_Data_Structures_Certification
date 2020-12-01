1acc56bd65e57fff715d0ca15000dd7999332731
LuisContrerasGlz <luis.fco.contreras@gmail.com>
2020-12-01 02:52:32 -0600

    updating function work file

--- Basic_JavaScript/functions.js
+++ Basic_JavaScript/functions.js
@@ -88,4 +88,19 @@ function timesFive(num) {
   }
   var answer = timesFive(5);
 
-  
+/*  everything to the right of the equal sign is resolved before the value is assigned. 
+This means we can take the return value of a function and assign it to a variable.*/
+
+//Call the processArg function with an argument of 7 and assign its return value to the variable processed.
+
+// Setup
+var processed = 0;
+
+function processArg(num) {
+  return (num + 3) / 5;
+}
+
+// Only change code below this line
+
+processed = processArg(7);
+

a62c866153a268e1b027dfaebabca13ffbf6e011
LuisContrerasGlz <luis.fco.contreras@gmail.com>
2020-12-20 09:57:15 -0600

    adding default

--- Basic_JavaScript/switchStatements.js
+++ Basic_JavaScript/switchStatements.js
@@ -30,3 +30,34 @@ function caseInSwitch(val) {
   }
   
   console.log(caseInSwitch(1));
+
+  /* In a switch statement you may not be able to specify all possible values as case statements. Instead, you can add the default statement which will be executed if no matching case statements are found. 
+  Think of it like the final else statement in an if/else chain.*/
+
+  /* Write a switch statement to set answer for the following conditions:
+  "a" - "apple"
+  "b" - "bird"
+  "c" - "cat"
+  default - "stuff" */
+
+  function switchOfStuff(val) {
+    var answer = "";
+    switch(val) {
+        case "a":
+          answer = "apple"
+          break;
+        case "b":
+          answer = "bird"
+          break;
+        case "c":
+            answer = "cat"
+            break;
+        default:
+                answer = "stuff"
+                break;
+      }
+      
+    return answer;
+  }
+  
+  switchOfStuff(1);

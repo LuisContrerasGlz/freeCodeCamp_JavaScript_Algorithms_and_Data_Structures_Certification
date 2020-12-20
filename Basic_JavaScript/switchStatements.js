595c22ce8f419fc4bc7fedeaed3439b9f7e70049
LuisContrerasGlz <luis.fco.contreras@gmail.com>
2020-12-20 10:38:15 -0600

    progress on switch

--- Basic_JavaScript/switchStatements.js
+++ Basic_JavaScript/switchStatements.js
@@ -61,3 +61,43 @@ function caseInSwitch(val) {
   }
   
   switchOfStuff(1);
+
+  /* If the break statement is omitted from a switch statement's case, the following case statement(s) are executed until a break is encountered.
+   If you have multiple inputs with the same output, you can represent them in a switch statement*/
+
+   /* 
+   Write a switch statement to set answer for the following ranges:
+   1-3 - "Low"
+   4-6 - "Mid"
+   7-9 - "High"
+   */
+
+  function sequentialSizes(val) {
+    var answer = "";
+   
+    switch(val) {
+        case 1:
+        case 2:
+        case 3:
+          answer = "Low"
+          break;
+        case 4:
+        case 5:
+        case 6:
+          answer = "Mid"
+          break;
+        case 7:
+        case 8:
+        case 9:
+            answer = "High"
+            break;
+      }
+      
+    return answer;
+  }
+  
+  sequentialSizes(1);
+
+  /*If you have many options to choose from, 
+  a switch statement can be easier to write than many chained if/else if statements*/
+  

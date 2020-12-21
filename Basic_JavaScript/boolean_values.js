684021088d3b79224ccf2ee9de3fd5f9a277d824
LuisContrerasGlz <luis.fco.contreras@gmail.com>
2020-12-21 00:35:43 -0600

    progress on program

--- Basic_JavaScript/boolean_values.js
+++ Basic_JavaScript/boolean_values.js
@@ -15,3 +15,19 @@ function welcomeToBooleans() {
   
     // Only change code above this line
   }
+
+  /* You may recall from Comparison with the Equality Operator that all comparison operators return a boolean true or false value.
+  Sometimes people use an if/else statement to do a comparison
+  But there's a better way to do this. Since === returns true or false, 
+  we can return the result of the comparison
+  */
+
+
+ function isLess(a, b) {
+  // Only change code below this line
+  return a < b 
+  // Only change code above this line
+}
+
+isLess(10, 15);
+

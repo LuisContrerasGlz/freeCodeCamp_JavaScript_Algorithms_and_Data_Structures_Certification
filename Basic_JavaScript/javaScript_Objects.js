1fd187a608f64899ba22adf462376bb2a42ceece
LuisContrerasGlz <luis.fco.contreras@gmail.com>
2020-12-29 01:24:50 -0600

    deleting from objects

--- Basic_JavaScript/javaScript_Objects.js
+++ Basic_JavaScript/javaScript_Objects.js
@@ -118,3 +118,19 @@ var myDog = {
   };
 
   myDog.bark = "woof";
+
+/* We can also delete properties from objects like this:
+delete ourDog.bark; */
+
+//Delete the "tails" property from myDog. You may use either dot or bracket notation.
+
+// Setup
+var myDog = {
+    "name": "Happy Coder",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["freeCodeCamp Campers"],
+    "bark": "woof"
+  };
+
+delete myDog.tails;

c76754c37825b39a0e77ac541c903cc621f00b4c
LuisContrerasGlz <luis.fco.contreras@gmail.com>
2021-01-01 15:54:39 -0600

    adding basic while loop

---
+++ Basic_JavaScript/loops.js
@@ -0,0 +1,14 @@
+/* You can run the same code multiple times by using a loop.
+The first type of loop is called a while loop because it runs "while" a specified condition is true and stops once that condition is no longer true.*/
+
+// Add the numbers 5 through 0 (inclusive) in descending order to myArray using a while loop.
+
+// Setup
+var myArray = [];
+var i = 5;
+while(i>=0){
+    myArray.push(i);
+    i--;
+}
+
+// Only change code below this line

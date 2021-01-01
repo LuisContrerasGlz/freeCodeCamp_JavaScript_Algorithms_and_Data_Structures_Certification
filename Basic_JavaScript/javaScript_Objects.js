255d1b2a6021d814a9742c963b6a6f68e716d0e8
LuisContrerasGlz <luis.fco.contreras@gmail.com>
2020-12-31 19:14:08 -0600

    accessing nested arrays

--- Basic_JavaScript/javaScript_Objects.js
+++ Basic_JavaScript/javaScript_Objects.js
@@ -220,3 +220,31 @@ var myStorage = {
   };
   
   var gloveBoxContents = myStorage.car.inside["glove box"]; 
+
+  // objects can contain both nested objects and nested arrays. Similar to accessing nested objects, Array bracket notation can be chained to access nested arrays.
+
+  //Retrieve the second tree from the variable myPlants using object dot and array bracket notation.
+
+  // Setup
+var myPlants = [
+    {
+      type: "flowers",
+      list: [
+        "rose",
+        "tulip",
+        "dandelion"
+      ]
+    },
+    {
+      type: "trees",
+      list: [
+        "fir",
+        "pine",
+        "birch"
+      ]
+    }
+  ];
+  
+  // Only change code below this line
+  
+  var secondTree = myPlants[1].list[1]; // Change this line

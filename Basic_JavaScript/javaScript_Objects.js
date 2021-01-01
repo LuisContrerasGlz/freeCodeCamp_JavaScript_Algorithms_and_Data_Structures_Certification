fd61449c21c4f6f6746c24e6750ca5f32379afdf
LuisContrerasGlz <luis.fco.contreras@gmail.com>
2020-12-31 19:06:52 -0600

    accessing nested objects

--- Basic_JavaScript/javaScript_Objects.js
+++ Basic_JavaScript/javaScript_Objects.js
@@ -204,3 +204,19 @@ function phoneticLookup(val) {
           ]
     }
   ];
+
+//The sub-properties of objects can be accessed by chaining together the dot or bracket notation.
+// Setup
+var myStorage = {
+    "car": {
+      "inside": {
+        "glove box": "maps",
+        "passenger seat": "crumbs"
+       },
+      "outside": {
+        "trunk": "jack"
+      }
+    }
+  };
+  
+  var gloveBoxContents = myStorage.car.inside["glove box"]; 

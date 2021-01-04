e69a54400a914c70e898071f423ea65a65c445cb
LuisContrerasGlz <luis.fco.contreras@gmail.com>
2021-01-04 01:49:51 -0600

    adding for loop

--- Basic_JavaScript/loops.js
+++ Basic_JavaScript/loops.js
@@ -11,4 +11,26 @@ while(i>=0){
     i--;
 }
 
-// Only change code below this line
+/* You can run the same code multiple times by using a loop.
+The most common type of JavaScript loop is called a for loop because it runs "for" a specific number of times.
+For loops are declared with three optional expressions separated by semicolons:
+
+for ([initialization]; [condition]; [final-expression])
+
+The initialization statement is executed one time only before the loop starts. 
+It is typically used to define and setup your loop variable.
+
+The condition statement is evaluated at the beginning of every loop iteration and will continue as long as it evaluates to true. When condition is false at the start of the iteration, the loop will stop executing. 
+This means if condition starts as false, your loop will never execute.
+
+The final-expression is executed at the end of each loop iteration, prior to the next condition check and is usually used to increment or decrement your loop counter.
+
+*/
+
+//Use a for loop to work to push the values 1 through 5 onto myArray.
+
+// Setup
+var myArray = [];
+for (var i = 1; i <= 5; i++) {
+    myArray.push(i);
+}

5da08b08d431ad41bfc30587bb94e83b64c56324
LuisContrerasGlz <luis.fco.contreras@gmail.com>
2021-01-04 02:26:41 -0600

    Nesting for loops

--- Basic_JavaScript/loops.js
+++ Basic_JavaScript/loops.js
@@ -76,4 +76,19 @@ for (var i = 0; i < myArr.length; i++) {
     total += myArr[i];
  }
 
+ //If you have a multi-dimensional array, you can use the same logic as the prior waypoint to loop through both the array and any sub-arrays. 
+//Modify function multiplyAll so that it returns the product of all the numbers in the sub-arrays of arr.
+
+function multiplyAll(arr) {
+    var product = 1;
+    
+    for (var i = 0; i < arr.length; i++){
+        for (var j = 0; j < arr[i].length; j++){
+            product *= arr[i][j];
+        }
+    }
+    return product;
+  }
+  
+  multiplyAll([[1,2],[3,4],[5,6,7]]);
 

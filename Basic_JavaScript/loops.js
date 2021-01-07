fb7c3fc6db41b16bb3ae50bb91e1fc7feb1c709b
LuisContrerasGlz <luis.fco.contreras@gmail.com>
2021-01-06 23:14:47 -0600

    recursion

--- Basic_JavaScript/loops.js
+++ Basic_JavaScript/loops.js
@@ -92,3 +92,31 @@ function multiplyAll(arr) {
   
   multiplyAll([[1,2],[3,4],[5,6,7]]);
 
+  /* The next type of loop you will learn is called a do...while loop. 
+  It is called a do...while loop because it will first do one pass of the code inside the loop no matter what, and then continue to run the loop while the specified condition evaluates to true.
+  */
+
+/* Change the while loop in the code to a do...while loop so the loop will push only the number 10 to myArray, 
+and i will be equal to 11 when your code has finished running.*/
+
+// Setup
+var myArray = [];
+var i = 10;
+
+do {
+    myArray.push(i);
+  i++;
+}
+while (i < 11) 
+
+/* Recursion is the concept that a function can be expressed in terms of itself. 
+Write a recursive function, sum(arr, n), that returns the sum of the first n elements of an array arr.*/
+
+function sum(arr, n) {
+    if(n <= 0) {
+      return 0;
+    } else {
+      return sum(arr, n - 1) + arr[n - 1];
+    }
+  }
+

e0085f0bb0c1a4434cfc094a25cba26da39c03e1
LuisContrerasGlz <luis.fco.contreras@gmail.com>
2021-01-10 01:52:36 -0600

    adding Ternary Operators

--- Basic_JavaScript/comparison.js
+++ Basic_JavaScript/comparison.js
@@ -223,6 +223,57 @@ function testLogicalOr(val) {
 
 testLogicalOr(15);
 
+/* The conditional operator, also called the ternary operator, can be used as a one line if-else expression.
+
+The syntax is:
+
+condition ? expression-if-true : expression-if-false;
+
+The following function uses an if-else statement to check a condition:
+
+function findGreater(a, b) {
+  if(a > b) {
+    return "a is greater";
+  }
+  else {
+    return "b is greater";
+  }
+}
+
+This can be re-written using the conditional operator:
+
+function findGreater(a, b) {
+  return a > b ? "a is greater" : "b is greater";
+}
+
+*/
+
+// Use the conditional operator in the checkEqual function to check if two numbers are equal or not. The function should return either "Equal" or "Not Equal".
+
+function checkEqual(a, b) {
+   return a === b ? "Equal" : "Not Equal";
+}
+
+checkEqual(1, 2);
+
+/* In the previous challenge, you used a single conditional operator. 
+You can also chain them together to check for multiple conditions.
+
+It is considered best practice to format multiple conditional operators such that each condition is on a separate line
+
+*/
+
+/*In the checkSign function, use multiple conditional operators - following the recommended format used in findGreaterOrEqual - to check if a number is positive, negative or zero. 
+The function should return "positive", "negative" or "zero".
+
+*/
+
+function checkSign(num) {
+  return num > 0 ? "positive" 
+  : num < 0 ? "negative" : "zero"
+}
+
+checkSign(10);
 
 
 

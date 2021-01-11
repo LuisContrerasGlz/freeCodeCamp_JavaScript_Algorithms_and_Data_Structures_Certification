5b4916a96e4f0039a8373ecf105404700a8d7432
LuisContrerasGlz <luis.fco.contreras@gmail.com>
2021-01-10 22:09:21 -0600

    ES6 Starting

---
+++ ES6/ES6.js
@@ -0,0 +1,14 @@
+/* ECMAScript is a standardized version of JavaScript with the goal of unifying the language's specifications and features. 
+As all major browsers and JavaScript-runtimes follow this specification, the term ECMAScript is interchangeable with the term JavaScript.
+
+The most recent standardized version is called ECMAScript 6 (ES6), released in 2015. 
+This new version of the language adds some powerful features, including:
+
+Arrow functions
+Classes
+Modules
+Promises
+Generators
+let and const
+
+*/

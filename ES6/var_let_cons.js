639f66a6724886f78010909b4d107856f21609dc
LuisContrerasGlz <luis.fco.contreras@gmail.com>
2021-01-12 03:48:56 -0600

    Object mutation

--- ES6/var_let_cons.js
+++ ES6/var_let_cons.js
@@ -80,4 +80,34 @@ function checkScope() {
   }
   printManyTimes("freeCodeCamp");
 
+  /* The const declaration has many use cases in modern JavaScript.
+
+  Some developers prefer to assign all their variables using const by default, unless they know they will need to reassign the value. Only in that case, they use let.
+  However, it is important to understand that objects (including arrays and functions) assigned to a variable using const are still mutable. Using the const declaration only prevents reassignment of the variable identifier.
+
+  const s = [5, 6, 7];
+  s = [1, 2, 3]; // throws error, trying to assign a const
+  s[2] = 45; // works just as it would with an array declared with var or let
+  console.log(s); // returns [5, 6, 45]
+
+  */
+
+  
+  //An array is declared as const s = [5, 7, 2]. Change the array to [2, 5, 7] using various element assignments.
+
+
+ const s = [5, 7, 2];
+ function editInPlace() {
+   // Only change code below this line
+ s[0] = 2;
+ s[1] = 5;
+ s[2] = 7;
+   // Using s = [2, 5, 7] would be invalid
+ 
+   // Only change code above this line
+ }
+ editInPlace();
+
+
+
   

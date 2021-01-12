ab603d0f7210689c9df0f8c553cc3d3d4caae15f
LuisContrerasGlz <luis.fco.contreras@gmail.com>
2021-01-11 23:40:49 -0600

    const

--- ES6/var_let_cons.js
+++ ES6/var_let_cons.js
@@ -53,3 +53,31 @@ function checkScope() {
     console.log('Function scope i is: ', i);
     return i;
   }
+
+  /* The keyword let is not the only new way to declare variables. In ES6, you can also declare variables using the const keyword.
+
+  const has all the awesome features that let has, with the added bonus that variables declared using const are read-only. 
+  They are a constant value, which means that once a variable is assigned with const, it cannot be reassigned.
+
+  trying to reassign a variable declared with const will throw an error. 
+  You should always name variables you don't want to reassign using the const keyword. 
+  This helps when you accidentally attempt to reassign a variable that is meant to stay constant. A common practice when naming constants is to use all uppercase letters, with words separated by an underscore.
+
+  Note: It is common for developers to use uppercase variable identifiers for immutable values and lowercase or camelCase for mutable values (objects and arrays). */
+
+  /* Change the code so that all variables are declared using let or const. Use let when you want the variable to change, and const when you want the variable to remain constant. 
+  Also, rename variables declared with const to conform to common practices, meaning constants should be in all caps.*/
+
+  function printManyTimes(str) {
+
+  
+    const SENTENCE = str + " is cool!";
+    for (let i = 0; i < str.length; i+=2) {
+      console.log(SENTENCE);
+    }
+  
+
+  }
+  printManyTimes("freeCodeCamp");
+
+  

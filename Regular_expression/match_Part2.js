e9971afa4927b98811a907f6ec0f8fe35ac36b2d
LuisContrerasGlz <luis.fco.contreras@gmail.com>
2021-01-26 20:04:38 -0600

    progress on match

--- Regular_expression/match_Part2.js
+++ Regular_expression/match_Part2.js
@@ -107,3 +107,62 @@ The shortcut to look for non-digit characters is \D. This is equal to the charac
 let movieName = "2001: A Space Odyssey";
 let noNumRegex = /\D/g;
 let result = movieName.match(noNumRegex).length;
+
+/*Usernames are used everywhere on the internet. They are what give users a unique identity on their favorite sites.
+
+You need to check all the usernames in a database. 
+Here are some simple rules that users have to follow when creating their username.
+
+Usernames can only use alpha-numeric characters.
+
+The only numbers in the username have to be at the end. There can be zero or more of them at the end. Username cannot start with the number.
+
+Username letters can be lowercase and uppercase.
+
+Usernames have to be at least two characters long. A two-character username can only use alphabet letters as characters.
+
+Change the regex userCheck to fit the constraints listed above.
+*/
+
+let username = "JackOfAllTrades";
+let userCheck = /^[a-z][a-z]+\d*$|^[a-z]\d\d+$/i;
+let result = userCheck.test(username);
+console.log(result)
+
+/*The challenges so far have covered matching letters of the alphabet and numbers. 
+You can also match the whitespace or spaces between letters.
+You can search for whitespace using \s, which is a lowercase s. 
+This pattern not only matches whitespace, but also carriage return, tab, form feed, and new line characters. 
+You can think of it as similar to the character class [ \r\t\f\n\v].
+
+let whiteSpace = "Whitespace. Whitespace everywhere!"
+let spaceRegex = /\s/g;
+whiteSpace.match(spaceRegex);
+// Returns [" ", " "]
+
+*/
+
+//Change the regex countWhiteSpace to look for multiple whitespace characters in a string.
+
+let sample = "Whitespace is important in separating words";
+let countWhiteSpace = /\s/g; 
+let result = sample.match(countWhiteSpace);
+
+/* You learned about searching for whitespace using \s, with a lowercase s. 
+You can also search for everything except whitespace.
+
+Search for non-whitespace using \S, which is an uppercase s. 
+This pattern will not match whitespace, carriage return, tab, form feed, and new line characters. 
+You can think of it being similar to the character class [^ \r\t\f\n\v].
+
+let whiteSpace = "Whitespace. Whitespace everywhere!"
+let nonSpaceRegex = /\S/g;
+whiteSpace.match(nonSpaceRegex).length; // Returns 32
+
+*/
+
+//Change the regex countNonWhiteSpace to look for multiple non-whitespace characters in a string.
+
+let sample = "Whitespace is important in separating words";
+let countNonWhiteSpace = /\S/g; // Change this line
+let result = sample.match(countNonWhiteSpace);

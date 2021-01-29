ef242ab12db7153b83cb078abbbfc81290c6ed85
LuisContrerasGlz <luis.fco.contreras@gmail.com>
2021-01-29 02:27:45 -0600

    finished debugging section

--- Debugging/debugging.js
+++ Debugging/debugging.js
@@ -102,3 +102,170 @@ const allSameQuotes = 'I\'ve had a perfectly wonderful evening, but this wasn\'t
 
 let innerHtml = "<p>Click here to <a href='#Home'>return home</a></p>";
 console.log(innerHtml);
+
+/* Branching programs, i.e. ones that do different things if certain conditions are met, rely on if, else if, and else statements in JavaScript. 
+The condition sometimes takes the form of testing whether a result is equal to a value.
+
+This logic is spoken (in English, at least) as "if x equals y, then ..." which can literally translate into code using the =, or assignment operator. 
+This leads to unexpected control flow in your program.
+
+As covered in previous challenges, the assignment operator (=) in JavaScript assigns a value to a variable name. 
+And the == and === operators check for equality (the triple === tests for strict equality, meaning both value and type are the same).
+
+The code below assigns x to be 2, which evaluates as true. 
+Almost every value on its own in JavaScript evaluates to true, except what are known as the "falsy" values: false, 0, "" (an empty string), NaN, undefined, and null.
+
+let x = 1;
+let y = 2;
+if (x = y) {
+  // this code block will run for any value of y (unless y were originally set as a falsy)
+} else {
+  // this code block is what should run (but won't) in this example
+}
+
+*/
+
+//Fix the condition so the program runs the right branch, and the appropriate value is assigned to result.
+
+let x = 7;
+let y = 9;
+let result = "to come";
+
+if(x === y) {
+  result = "Equal!";
+} else {
+  result = "Not equal!";
+}
+
+console.log(result);
+
+/* When a function or method doesn't take any arguments, you may forget to include the (empty) opening and closing parentheses when calling it. 
+Often times the result of a function call is saved in a variable for other use in your code.
+This error can be detected by logging variable values (or their types) to the console and seeing that one is set to a function reference, instead of the expected value the function returns.
+
+The variables in the following example are different:
+
+function myFunction() {
+  return "You rock!";
+}
+let varOne = myFunction; // set to equal a function
+let varTwo = myFunction(); // set to equal the string "You rock!"
+
+*/
+
+//Fix the code so the variable result is set to the value returned from calling the function getNine.
+
+function getNine() {
+    let x = 6;
+    let y = 3;
+    return x + y;
+  }
+  
+  let result = getNine();
+  console.log(result);
+
+/*Continuing the discussion on calling functions, the next bug to watch out for is when a function's arguments are supplied in the incorrect order. 
+If the arguments are different types, such as a function expecting an array and an integer, this will likely throw a runtime error. 
+If the arguments are the same type (all integers, for example), then the logic of the code won't make sense. 
+Make sure to supply all required arguments, in the proper order to avoid these issues.
+*/
+
+//The function raiseToPower raises a base to an exponent. Unfortunately, it's not called properly - fix the code so the value of power is the expected 8.
+
+function raiseToPower(b, e) {
+    return Math.pow(b, e);
+  }
+  
+  let base = 2;
+  let exp = 3;
+  let power = raiseToPower(base, exp);
+  console.log(power);
+
+/*Off by one errors (sometimes called OBOE) crop up when you're trying to target a specific index of a string or array (to slice or access a segment), or when looping over the indices of them.
+JavaScript indexing starts at zero, not one, which means the last index is always one less than the length of the item. 
+If you try to access an index equal to the length, the program may throw an "index out of range" reference error or print undefined.
+
+When you use string or array methods that take index ranges as arguments, it helps to read the documentation and understand if they are inclusive (the item at the given index is part of what's returned) or not. 
+Here are some examples of off by one errors:
+
+let alphabet = "abcdefghijklmnopqrstuvwxyz";
+let len = alphabet.length;
+for (let i = 0; i <= len; i++) {
+  // loops one too many times at the end
+  console.log(alphabet[i]);
+}
+for (let j = 1; j < len; j++) {
+  // loops one too few times and misses the first character at index 0
+  console.log(alphabet[j]);
+}
+for (let k = 0; k < len; k++) {
+  // Goldilocks approves - this is just right
+  console.log(alphabet[k]);
+}
+
+*/
+
+//Fix the two indexing errors in the following function so all the numbers 1 through 5 are printed to the console.
+
+function countToFive() {
+    let firstFive = "12345";
+    let len = firstFive.length;
+    // Only change code below this line
+    for (let i = 0; i < len; i++) {
+    // Only change code above this line
+      console.log(firstFive[i]);
+    }
+  }
+  
+  countToFive();
+
+/*Sometimes it's necessary to save information, increment counters, or re-set variables within a loop.
+A potential issue is when variables either should be reinitialized, and aren't, or vice versa. 
+This is particularly dangerous if you accidentally reset the variable being used for the terminal condition, causing an infinite loop.
+
+Printing variable values with each cycle of your loop by using console.log() can uncover buggy behavior related to resetting, or failing to reset a variable.
+The following function is supposed to create a two-dimensional array with m rows and n columns of zeroes. Unfortunately, it's not producing the expected output because the row variable isn't being reinitialized (set back to an empty array) in the outer loop. Fix the code so it returns a correct 3x2 array of zeroes, which looks like [[0, 0], [0, 0], [0, 0]].
+
+*/
+
+function zeroArray(m, n) {
+    let newArray = [];
+    for (let i = 0; i < m; i++) {
+      let row = []; /* <-----  row has been declared inside the outer loop. 
+       Now a new row will be initialised during each iteration of the outer loop allowing 
+       for the desired matrix. */
+      for (let j = 0; j < n; j++) {
+        row.push(0);
+      }
+      newArray.push(row);
+    }
+    return newArray;
+  }
+  let matrix = zeroArray(3, 2);
+  console.log(matrix);
+
+  /*The final topic is the dreaded infinite loop. 
+  Loops are great tools when you need your program to run a code block a certain number of times or until a condition is met, but they need a terminal condition that ends the looping. 
+  Infinite loops are likely to freeze or crash the browser, and cause general program execution mayhem, which no one wants.
+
+  There was an example of an infinite loop in the introduction to this section - it had no terminal condition to break out of the while loop inside loopy(). Do NOT call this function!
+  function loopy() {
+  while(true) {
+    console.log("Hello, world!");
+  }
+}
+
+It's the programmer's job to ensure that the terminal condition, which tells the program when to break out of the loop code, is eventually reached. 
+One error is incrementing or decrementing a counter variable in the wrong direction from the terminal condition.
+Another one is accidentally resetting a counter or index variable within the loop code, instead of incrementing or decrementing it.
+*/
+
+/*The myFunc() function contains an infinite loop because the terminal condition i != 4 will never evaluate to false (and break the looping) - i will increment by 2 each pass, and jump right over 4 since i is odd to start. 
+Fix the comparison operator in the terminal condition so the loop only runs for i less than or equal to 4.
+*/
+
+function myFunc() {
+    for (let i = 1; i <= 4; i += 2) {
+      console.log("Still going!");
+    }
+  }

3db4e6461c2615495193f3d086b3d5d132fc839e
LuisContrerasGlz <luis.fco.contreras@gmail.com>
2021-02-10 22:16:17 -0600

    progress

--- Basic_Algorithm_Scripting/Algorithms1.js
+++ Basic_Algorithm_Scripting/Algorithms1.js
@@ -96,4 +96,25 @@ function largestOfFour(arr) {
   The string is indexed into using the variable i, whilst i is less than the length of the string. The index of each space is found within the string, and this is used as the length of each word.
   */
 
+  /* Check if a string (first argument, str) ends with the given target string (second argument, target).
+
+  This challenge can be solved with the .endsWith() method, which was introduced in ES2015. But for the purpose of this challenge, we would like you to use one of the JavaScript substring methods instead.*/
+
+  function confirmEnding(str, target) {
+    return str.slice(-target.length) === target
+  }
+  
+  confirmEnding("Bastian", "n");
+
+  //Repeat a given string str (first argument) for num times (second argument). Return an empty string if num is not a positive number. For the purpose of this challenge, do not use the built-in .repeat() method.
+  function repeatStringNumTimes(str, num) {
+    var accumulatedStr = "";
+  
+    while (num > 0) {
+      accumulatedStr += str;
+      num--;
+    }
+  
+    return accumulatedStr;
+  }
 

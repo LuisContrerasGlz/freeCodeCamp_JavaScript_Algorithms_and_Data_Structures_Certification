109ecc2f260f5d5aab082d0e665878a5708c80bf
LuisContrerasGlz <luis.fco.contreras@gmail.com>
2021-02-18 21:44:05 -0600

    adding Slice and Splice

--- Basic_Algorithm_Scripting/Algorithms2.js
+++ Basic_Algorithm_Scripting/Algorithms2.js
@@ -23,3 +23,23 @@ function titleCase(str) {
   }
   
   titleCase("I'm a little tea pot");
+
+  /*You are given two arrays and an index.
+
+Copy each element of the first array into the second array, in order.
+
+Begin inserting elements at index n of the second array.
+
+Return the resulting array. The input arrays should remain the same after the function runs.*/
+
+function frankenSplice(arr1, arr2, n) {
+    let result = [];
+
+    result.push(...arr2.slice(0, n));
+    result.push(...arr1);
+    result.push(...arr2.slice(n, arr2.length));
+
+    return result;
+  }
+  
+  frankenSplice([1, 2, 3], [4, 5, 6], 1);

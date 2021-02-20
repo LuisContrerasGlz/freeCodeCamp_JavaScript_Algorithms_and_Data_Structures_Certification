140151f46e0a2890566ce5bcf44df5121e2f281b
LuisContrerasGlz <luis.fco.contreras@gmail.com>
2021-02-19 23:53:11 -0600

    adding Falsy Bouncer problem

--- Basic_Algorithm_Scripting/Algorithms2.js
+++ Basic_Algorithm_Scripting/Algorithms2.js
@@ -43,3 +43,31 @@ function frankenSplice(arr1, arr2, n) {
   }
   
   frankenSplice([1, 2, 3], [4, 5, 6], 1);
+
+  /*Remove all falsy values from an array.
+
+   Falsy values in JavaScript are false, null, 0, "", undefined, and NaN.
+
+   Hint: Try converting each value to a Boolean.*/
+
+   function bouncer(arr) {
+    return arr;
+   }
+  
+   bouncer([7, "ate", "", false, 9]);
+
+    /*Remove all falsy values from an array.
+
+    Falsy values in JavaScript are false, null, 0, "", undefined, and NaN.
+
+    Hint: Try converting each value to a Boolean.
+
+     */
+
+    function bouncer(arr) {
+        let newArray = [];
+        for (var i = 0; i < arr.length; i++) {
+          if (arr[i]) newArray.push(arr[i]);
+        }
+        return newArray;
+      }

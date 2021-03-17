db1f642cfc4be0975ca7da0132214ca576c0025f
LuisContrerasGlz <luis.fco.contreras@gmail.com>
2021-03-17 01:22:43 -0600

    continue with functional programming problems

--- Functional_Programming/functional_programming2.js
+++ Functional_Programming/functional_programming2.js
@@ -260,3 +260,25 @@ var watchList = [
     return averageRating;
   }
   console.log(getRating(watchList));
+
+/* Now that you have worked through a few challenges using higher-order functions like map(), filter(), and reduce(), you now get to apply them to solve a more complex challenge.
+
+We have defined a function named squareList. You need to complete the code for the squareList function using any combination of map(), filter(), and reduce() so that it returns a new array containing only the square of only the positive integers (decimal numbers are not integers) when an array of real numbers is passed to it. An example of an array containing only real numbers is [-3, 4.8, 5, 3, -3.2].
+
+Note: Your function should not use any kind of for or while loops or the forEach() function. */
+
+const squareList = arr => {
+    let positiveIntegersSquared = arr.filter(function(num){
+        if (Number.isInteger(num) && num > 0) {
+            return num;
+        }
+    }).map(function(num){
+        return num * num;
+    }) 
+
+    return positiveIntegersSquared;
+  };
+  
+  const squaredIntegers = squareList([-3, 4.8, 5, 3, -3.2]);
+  console.log(squaredIntegers);
+

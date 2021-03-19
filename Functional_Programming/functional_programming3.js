f93287006f9e3ba0c32d9b2b762ccfa6b8e720fa
LuisContrerasGlz <luis.fco.contreras@gmail.com>
2021-03-19 17:51:49 -0600

    completed functional programming section

--- Functional_Programming/functional_programming3.js
+++ Functional_Programming/functional_programming3.js
@@ -74,3 +74,150 @@ function splitify(str) {
     return str.split(/\W/);
   }
 splitify("Hello World,I-am code");
+
+/*The join method is used to join the elements of an array together to create a string. It takes an argument for the delimiter that is used to separate the array elements in the string.
+
+Here's an example:
+
+var arr = ["Hello", "World"];
+var str = arr.join(" ");
+
+str would have a value of the string Hello World. */
+
+/*Use the join method (among others) inside the sentensify function to make a sentence from the words in the string str. 
+The function should return a string. 
+For example, I-like-Star-Wars would be converted to I like Star Wars. 
+For this challenge, do not use the replace method. */
+
+function sentensify(str) {
+    let disectedResult = str.split(/\W/);
+    let joinedResult = disectedResult.join(" ");
+
+    return joinedResult;
+
+  }
+  sentensify("May-the-force-be-with-you");
+
+/*The last several challenges covered a number of useful array and string methods that follow functional programming principles. 
+We've also learned about reduce, which is a powerful method used to reduce problems to simpler forms. 
+From computing averages to sorting, any array operation can be achieved by applying it. 
+Recall that map and filter are special cases of reduce.
+
+Let's combine what we've learned to solve a practical problem.
+
+Many content management sites (CMS) have the titles of a post added to part of the URL for simple bookmarking purposes. 
+For example, if you write a Medium post titled Stop Using Reduce, it's likely the URL would have some form of the title string in it (.../stop-using-reduce).
+You may have already noticed this on the freeCodeCamp site.
+
+Fill in the urlSlug function so it converts a string title and returns the hyphenated version for the URL. You can use any of the methods covered in this section, and don't use replace. Here are the requirements:
+
+The input is a string with spaces and title-cased words
+
+The output is a string with the spaces between words replaced by a hyphen (-)
+
+The output should be all lower-cased letters
+
+The output should not have any spaces */
+
+// the global variable
+var globalTitle = "Winter Is Coming";
+
+function urlSlug(title) {
+  return title
+    .toLowerCase()
+    .trim()
+    .split(/\s+/)
+    .join("-");
+}
+
+var winterComing = urlSlug(globalTitle); // Should be "winter-is-coming"
+
+/* The every method works with arrays to check if every element passes a particular test. It returns a Boolean value - true if all values meet the criteria, false if not.
+For example, the following code would check if every element in the numbers array is less than 10:
+
+var numbers = [1, 5, 8, 0, 10, 11];
+numbers.every(function(currentValue) {
+  return currentValue < 10;
+});
+
+The every method would return false here. */
+
+//Use the every method inside the checkPositive function to check if every element in arr is positive. The function should return a Boolean value.
+
+function checkPositive(arr) {
+    return arr.every(function(value) {
+      return value > 0;
+    });
+  }
+  checkPositive([1, 2, 3, -4, 5]);
+
+/*The some method works with arrays to check if any element passes a particular test. 
+It returns a Boolean value - true if any of the values meet the criteria, false if not.
+
+For example, the following code would check if any element in the numbers array is less than 10:
+
+var numbers = [10, 50, 8, 220, 110, 11];
+numbers.some(function(currentValue) {
+  return currentValue < 10;
+});
+
+The some method would return true. */
+
+//Use the some method inside the checkPositive function to check if any element in arr is positive. The function should return a Boolean value.
+
+function checkPositive(arr) {
+    return arr.some(function(value) {
+          return value > 0;
+        });
+    }
+    checkPositive([1, 2, 3, -4, 5]);
+
+/*The arity of a function is the number of arguments it requires. Currying a function means to convert a function of N arity into N functions of arity 1.
+
+In other words, it restructures a function so it takes one argument, then returns another function that takes the next argument, and so on.
+
+Here's an example:
+
+function unCurried(x, y) {
+  return x + y;
+}
+
+function curried(x) {
+  return function(y) {
+    return x + y;
+  }
+}
+
+const curried = x => y => x + y
+
+curried(1)(2)
+
+curried(1)(2) would return 3.
+
+This is useful in your program if you can't supply all the arguments to a function at one time. You can save each function call into a variable, which will hold the returned function reference that takes the next argument when it's available. Here's an example using the curried function in the example above:
+
+var funcForY = curried(1);
+console.log(funcForY(2)); // 3
+
+Similarly, partial application can be described as applying a few arguments to a function at a time and returning another function that is applied to more arguments. Here's an example:
+
+function impartial(x, y, z) {
+  return x + y + z;
+}
+var partialFn = impartial.bind(this, 1, 2);
+partialFn(10); // 13 */
+
+//Fill in the body of the add function so it uses currying to add parameters x, y, and z.
+
+function add(x) {
+    // Add your code below this line
+    return function(y) {
+      return function(z) {
+        return x + y + z;
+      };
+    };
+    // Add your code above this line
+  }
+  add(10)(20)(30);
+
+

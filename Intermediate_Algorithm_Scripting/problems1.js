a449ca0809d770d7537351d96fd752e31b09e36e
LuisContrerasGlz <luis.fco.contreras@gmail.com>
2021-03-21 22:37:14 -0600

    continue with problems

--- Intermediate_Algorithm_Scripting/problems1.js
+++ Intermediate_Algorithm_Scripting/problems1.js
@@ -94,3 +94,68 @@ function whatIsInAName(collection, source) {
     ],
     { last: "Capulet" }
   );
+
+//Convert a string to spinal case. Spinal case is all-lowercase-words-joined-by-dashes.
+
+function spinalCase(str) {
+  // Create a variable for the white space and underscores.
+  var regex = /\s+|_+/g;
+
+  // Replace low-upper case to low-space-uppercase
+  str = str.replace(/([a-z])([A-Z])/g, "$1 $2");
+
+  // Replace space and underscore with -
+  return str.replace(regex, "-").toLowerCase();
+}
+
+// test here
+spinalCase("This Is Spinal Tap");
+
+/*Pig Latin is a way of altering English Words. The rules are as follows:
+
+- If a word begins with a consonant, take the first consonant or consonant cluster, move it to the end of the word, and add ay to it.
+
+- If a word begins with a vowel, just add way at the end.
+
+Translate the provided string to Pig Latin. Input strings are guaranteed to be English words in all lowercase.*/
+
+function translatePigLatin(str) {
+    let consonantRegex = /^[^aeiou]+/;
+    let myConsonants = str.match(consonantRegex);
+    return myConsonants !== null
+      ? str
+          .replace(consonantRegex, "")
+          .concat(myConsonants)
+          .concat("ay")
+      : str.concat("way");
+  }
+  
+  translatePigLatin("consonant");
+
+/*Perform a search and replace on the sentence using the arguments provided and return the new sentence.
+
+First argument is the sentence to perform the search and replace on.
+
+Second argument is the word that you will be replacing (before).
+
+Third argument is what you will be replacing the second argument with (after).
+
+Note: Preserve the case of the first character in the original word when you are replacing it. 
+For example if you mean to replace the word Book with the word dog, it should be replaced as Dog */
+
+function myReplace(str, before, after) {
+    // Find index where before is on string
+    var index = str.indexOf(before);
+    // Check to see if the first letter is uppercase or not
+    if (str[index] === str[index].toUpperCase()) {
+      // Change the after word to be capitalized before we use it.
+      after = after.charAt(0).toUpperCase() + after.slice(1);
+    } else {
+      // Change the after word to be uncapitalized before we use it.
+      after = after.charAt(0).toLowerCase() + after.slice(1);
+    }
+    // Now replace the original str with the edited one.
+    str = str.replace(before, after);
+  
+    return str;
+  }

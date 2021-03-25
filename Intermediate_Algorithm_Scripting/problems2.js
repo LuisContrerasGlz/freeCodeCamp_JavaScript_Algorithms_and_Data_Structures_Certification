5b6ffa68f1a1075d6d9cd20ce239d93682668c3b
LuisContrerasGlz <luis.fco.contreras@gmail.com>
2021-03-25 13:23:38 -0600

    completed Intermediate Algorithm Scripting section

--- Intermediate_Algorithm_Scripting/problems2.js
+++ Intermediate_Algorithm_Scripting/problems2.js
@@ -104,4 +104,48 @@ function steamrollArray(arr) {
   
   steamrollArray([1, [2], [3, [[4]]]]);
 
+/*Return an English translated sentence of the passed binary string.
+
+The binary string will be space separated.*/
+
+function binaryAgent(str) {
+    let decipheredMessage = "";
+
+    str.split(" ").forEach(binaryDataPoint => {
+        let characterCode = parseInt(binaryDataPoint, 2);
+        let decipheredLetter = String.fromCharCode(characterCode);
+        decipheredMessage += decipheredLetter;
+    });
+
+
+    return decipheredMessage;
+  }
   
+  let binaryData = "01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111";
+  let result = binaryAgent(binaryData);
+  console.log(result);
+
+/*Check if the predicate (second argument) is truthy on all elements of a collection (first argument).
+
+In other words, you are given an array collection of objects. The predicate pre will be an object property and you need to return true if its value is truthy. Otherwise, return false.
+
+In JavaScript, truthy values are values that translate to true when evaluated in a Boolean context.
+
+Remember, you can access object properties through either dot notation or [] notation. */
+
+function truthCheck(collection, pre) {
+    return collection.every(function(element) {
+      return element.hasOwnProperty(pre) && Boolean(element[pre]);
+    });
+  }
+  
+  // test here
+  truthCheck(
+    [
+      { user: "Tinky-Winky", sex: "male" },
+      { user: "Dipsy", sex: "male" },
+      { user: "Laa-Laa", sex: "female" },
+      { user: "Po", sex: "female" }
+    ],
+    "sex"
+  );
